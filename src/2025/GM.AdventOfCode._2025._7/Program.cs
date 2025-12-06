c2221c2e9d2dc67b86ace5903379f7d52b6178cb
Gregor Mohorko <grega@mohorko.info>
2025-12-06 07:05:59 +0100

    chore: Prepare project

---
+++ src/2025/GM.AdventOfCode._2025._7/Program.cs
@@ -0,0 +1,19 @@
+﻿using GM.Utility;
+
+/*
+const string inputFileName = "input.txt";
+/*/
+const string inputFileName = "inputExample.txt";
+//*/
+
+
+string[] inputLines = await File.ReadAllLinesAsync(inputFileName);
+
+
+
+
+
+// 
+Console.WriteLine($"Part One: ");
+// 
+Console.WriteLine($"Part Two: ");
